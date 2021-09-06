f779cceb24400a84dfaee56db8104126e3783b4f
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-09-06 18:20:22 +0300

    added a view function for the search

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,7 @@
+from django.db.models import query
 from gallery.models import Category, Image
 from django.shortcuts import render
+from django.db.models import Q
 
 # Create your views here.
 def images_all(request):
@@ -7,17 +9,27 @@ def images_all(request):
     binamin = Category.all_categories()
     return render(request,'all-images/mixed.html', {"store":store},)
 
-def details(request):
-    pass
-
+def details(request,pk):
+    eachImage = Image.objects.get(id=pk)
+    return render(request,'all-images/each.html',{"eachImage":eachImage})
 def search(request):
-    q = request.GET.get('q',None)
-    items = ''
-    if q is None or q is "":
-        display = Image.objects.all()
-    elif q is not None:
-        display = Image.objects.filter(name__icontains = q)
-    return render(request,'all-images/search.html',{'display':display})
+    
+    if request.method == "GET":
+            query = request.GET.get('q')
+            if query:
+                display = Image.objects.filter(Q(name__icontains = query)|Q(category__pic_type__icontains = query)|Q(location__location__icontains = query))
+                return render(request,'all-images/search.html', {"display":display})
+            else:
+                print("no data found")
+                return render(request,'all-images/search.html',{})
+    
+    # q = request.GET.get('q',None)
+    # items = ''
+    # if q is None or q is "":
+    #     display = Image.objects.all()
+    # elif q is not None:
+    #     display = Image.objects.filter(name__icontains = q)
+    # return render(request,'all-images/search.html',{'display':display})
     
     
     # TRY 2
